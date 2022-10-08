df72f0ad6acb51667c71f7168111d02bae3791ef
gift56 <efeasiughu@gmail.com>
2022-10-08 13:55:29 +0100

    giving assignment

--- main.js
+++ main.js
@@ -30,3 +30,15 @@ let x = 10;
 
 console.log(x);
 */
+
+// Assignment on javascript variables
+
+/*
+1. Create a variable using const and the unique name the unique name should be in camelcase and the value should be PI number
+2. Create a variable using any keyword and assign it to an arithmetics operation which divides 22 from 7.
+3. Create a variable using any keyword and assign it to an Array on countries.
+4. Create a varible using let, and reassign it down below to an array of ages.
+5. Create a varible using const and assign to name of two people join those name using arithmetics (+) sign
+6. lastly console.log(each and every number above) and display them on the webpage.
+
+*/
