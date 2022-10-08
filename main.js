0687827f6479eb3c7d232e02cd2a987589e3bc56
gift56 <efeasiughu@gmail.com>
2022-10-08 13:38:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// assign a varible using const
+
+/*
+const cars = ["Toyota", "Vovo", "BMW"];
+
+console.log(cars);*/
+
+// assigning varible using var
+
+/*
+var x = 10;
+
+{
+    x = 2;
+}
+
+console.log(x);
+*/
+
+// assigning varible using let
+
+/*
+let x = 10;
+
+{
+  let x = 2;
+
+  console.log(x);
+}
+
+console.log(x);
+*/
